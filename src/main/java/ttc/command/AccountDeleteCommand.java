fbf10b36b2d5e0fcfcd23dd82f4d2d03e83564c1
IshidaKazuki <nyanpoko1005@gmail.com>
2016-01-22 22:36:07 +0900

    アカウント削除jsp表示されないので動くかわからない動くといいな

--- src/main/java/ttc/command/AccountDeleteCommand.java
+++ src/main/java/ttc/command/AccountDeleteCommand.java
@@ -5,45 +5,36 @@ import ttc.context.ResponseContext;
 
 import ttc.util.MySqlConnectionManager;
 
-import ttc.exception.IntegrationException;
 import ttc.exception.BusinessLogicException;
-
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-
+import ttc.exception.IntegrationException;
 
 import ttc.util.factory.AbstractDaoFactory;
 import ttc.dao.AbstractDao;
-import ttc.bean.UserBean;
 
-import java.util.Calendar;
-import java.text.SimpleDateFormat;
+import java.util.Map;
+import java.util.HashMap;
 
-public class AdminAuthorizationCommand extends AbstractCommand{
-
-
+public class AccountDeleteCommand extends AbstractCommand{
     public ResponseContext execute(ResponseContext resc)throws BusinessLogicException{
         try{
             RequestContext reqc = getRequestContext();
 
-			String targetId = reqc.getParameter("targetId")[0];
-
-            MySqlConnectionManager.getInstance().beginTransaction();
-            AbstractDaoFactory factory = AbstractDaoFac tory.getFactory("users");
-            AbstractDao dao = factory.getAbstractDao();
+            String userId = reqc.getParameter("userId")[0];
+            String status="3";
 
             Map params = new HashMap();
-            params.put("userId",targetId);
-            params.put("adminFlag","1");
+            params.put("userId",userId);
+            params.put("userStatus",status);
+            MySqlConnectionManager.getInstance().beginTransaction();
 
+            AbstractDaoFactory factory = AbstractDaoFactory.getFactory("user");
+            AbstractDao dao = factory.getAbstractDao();
             dao.update(params);
-
             MySqlConnectionManager.getInstance().commit();
             MySqlConnectionManager.getInstance().closeConnection();
 
 
-            resc.setTarget("adminauresult");
+            resc.setTarget("accountdeleteresult");
 
             return resc;
         }catch(IntegrationException e){
