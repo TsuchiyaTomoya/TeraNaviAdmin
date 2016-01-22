c7dac67cdadc206d517a41cb07f72a25222eeeb1
MasakiFukuda <masaki1014@hotmail.co.jp>
2016-01-22 15:14:48 +0900

    ユニークキー生成用のutilクラスを追加 UniqueKeyGenerator.generateKeys(生成する数) で生成できます(戻り値はList型)

---
+++ src/main/java/ttc/util/UniqueKeyGenerator.java
@@ -0,0 +1,64 @@
+package ttc.util;
+
+import java.util.UUID;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public abstract class UniqueKeyGenerator{
+    public static List generateKeys(int count){
+        List result = new ArrayList();
+        
+        for(int i = 0;i < count;i++){
+            System.out.println();
+            
+            System.out.println((i+1)+"番目のユニークキーを生成します");
+            
+            String uId = UUID.randomUUID().toString();
+
+            
+            System.out.println("uId = "+uId);
+
+            MessageDigest md5 = null;
+            try{
+                md5 = MessageDigest.getInstance("MD5");
+            }catch(NoSuchAlgorithmException e){
+                e.printStackTrace();
+            }
+            md5.update(uId.getBytes());
+            byte[] hash = md5.digest();
+            
+            String mdStr = hexString(hash);
+            
+            System.out.println("生成されたハッシュキー : "+mdStr);
+            
+            
+            result.add(mdStr);
+        }
+        
+        return result;
+    }
+    
+    private static String hexString(byte[] bin){
+        String str = "";
+        int size = bin.length;
+        
+        for(int i = 0;i < size;i++){
+            int n = bin[i];
+            if(i < 0){
+                n += 256;
+            }
+            String hex = Integer.toHexString(n);
+            
+            if(hex.length() == 1){
+                hex = "0" + hex;
+            }
+            
+            str += hex;
+        }
+        
+        return str;
+    }
+}
